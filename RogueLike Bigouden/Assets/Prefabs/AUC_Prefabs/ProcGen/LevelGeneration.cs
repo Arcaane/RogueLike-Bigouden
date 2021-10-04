cf81a846e8ce4e9bbc87d5a5949323621f381bae
Aurélien <71558277+Arcaane@users.noreply.github.com>
2021-10-04 18:55:39 +0200

    2st itération // Still bugs

--- RogueLike Bigouden/Assets/Prefabs/AUC_Prefabs/ProcGen/LevelGeneration.cs
+++ RogueLike Bigouden/Assets/Prefabs/AUC_Prefabs/ProcGen/LevelGeneration.cs
@@ -7,7 +7,7 @@ using Random = UnityEngine.Random;
 public class LevelGeneration : MonoBehaviour
 {
     public Transform[] startingPositions;
-    public GameObject[] rooms; // 0 = LR , 1 = LRB, 2 = LRT, 3 = LRTB
+    public GameObject[] rooms;
     public List<GameObject> lastRoomInstantiate;
 
     private int direction;
@@ -52,90 +52,121 @@ public class LevelGeneration : MonoBehaviour
     {
         if (direction == 1 || direction == 2) // Move Right
         {
+            Debug.Log("Salle à droite");
             if (transform.position.x < maxX)
             {
                 upCounter = 0;
                 Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
                 transform.position = newPos;
-                
-                int rand = Random.Range(0, rooms.Length);
-                Instantiate(rooms[rand], transform.position, Quaternion.identity);
-                lastRoomInstantiate.Add(rooms[rand]);
-                
-                
-                direction = Random.Range(1, 6);
+
+                int rand = Random.Range(0, rooms.Length); // Random de la salle générée
+                if (rand == 0 || rand == 1)
+                {
+                    rand = 3;
+                    Instantiate(rooms[rand], transform.position, Quaternion.identity); // Instansiate de la salle
+                    lastRoomInstantiate.Add(rooms[rand]);
+                    //Debug.Log("Salle instansiate : ")
+                }
+                else if (rand == 2 || rand == 5)
+                {
+                    rand = 4;
+                    Instantiate(rooms[rand], transform.position, Quaternion.identity); // Instansiate de la salle
+                    lastRoomInstantiate.Add(rooms[rand]);
+                }
+                else
+                {
+                    Instantiate(rooms[rand], transform.position, Quaternion.identity); // Instansiate de la salle
+                    lastRoomInstantiate.Add(rooms[rand]);
+                }
+
+                direction = Random.Range(1, 6); // Random pour la prochaine direction : Si gauche -> droite ou haut 
                 if (direction == 3)
                     direction = 2;
                 else if (direction == 4)
                     direction = 5;
+                Debug.Log("Prochaine direction : " + direction);
             }
-            else
+            else // va vers le haut si pas possible d'aller à droite
             {
                 direction = 5;
             }
         }
-        else if(direction == 3 || direction == 4) // Move Left
+        else if (direction == 3 || direction == 4) // Move Left
         {
             if (transform.position.x > minX)
             {
                 upCounter = 0;
                 Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
                 transform.position = newPos;
-                
-                int rand = Random.Range(0, rooms.Length);
+
+                int rand = Random.Range(4, 7); // Random pour une salle compatible avec le mouvement à gauche (ouverte à droite)
                 Instantiate(rooms[rand], transform.position, Quaternion.identity);
                 lastRoomInstantiate.Add(rooms[rand]);
 
-                direction = Random.Range(3, 6);
+                direction = Random.Range(3, 6); // Random pour la prochaine direction : Pas de droite
+                Debug.Log("Prochaine direction : " + direction);
             }
-            else {
-                direction = 5;
+            else
+            {
+                direction = 5; // va vers le haut si pas possible d'aller à gauche
+                Debug.Log("Prochaine direction : " + direction);
             }
         }
-        
-        
-        else if (direction == 5) { // Move UP
+        else if (direction == 5)
+        { 
+            Debug.Log("Salle haut");
+            // Move UP
             upCounter++;
             if (transform.position.y < maxY)
             {
-                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
-                
-                if (roomDetection.GetComponent<RoomType>().type == 3 &&
-                    roomDetection.GetComponent<RoomType>().type == 0)
+                if (lastRoomInstantiate[lastRoomInstantiate.Count - 1].GetComponent<RoomType>().type == 0
+                    || lastRoomInstantiate[lastRoomInstantiate.Count - 1].GetComponent<RoomType>().type == 3
+                    || lastRoomInstantiate[lastRoomInstantiate.Count - 1].GetComponent<RoomType>().type == 5
+                    || lastRoomInstantiate[lastRoomInstantiate.Count - 1].GetComponent<RoomType>().type == 6)
                 {
+                    Debug.Log("Salle précédente valide");
                     if (upCounter >= 2)
                     {
-                        roomDetection.GetComponent<RoomType>().RoomDestruction();
-                        Instantiate(rooms[1], transform.position, Quaternion.identity);
-                        lastRoomInstantiate.Add(rooms[1]);
-                    }
-                    else
-                    {
-                        roomDetection.GetComponent<RoomType>().RoomDestruction();
-                        
-                        var randTopRoom = Random.Range(1, 4);
-                        if (randTopRoom == 2)
-                            randTopRoom = 1;
-
-                        Instantiate(rooms[randTopRoom], transform.position, Quaternion.identity);
-                        lastRoomInstantiate.Add(rooms[randTopRoom]);
-                        
+                        Vector2 newPos = new Vector2(transform.position.x, transform.position.y + moveAmount);
+                        transform.position = newPos;
+                    
+                        if (transform.position.x != maxX && transform.position.x != minX)
+                        {
+                            int rand = Random.Range(0,3);
+                            Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                            lastRoomInstantiate.Add(rooms[rand]);
+                        }
+                        else if (transform.position.x == minX) // Tout a gauche
+                        {
+                            int rand = Random.Range(0,3);
+                            while (rand == 1)
+                            {
+                                rand = Random.Range(0, 3);
+                            }
+                            Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                            lastRoomInstantiate.Add(rooms[rand]);
+                        }
+                        else if (transform.position.x == maxX) // Tout a droite
+                        {
+                            int rand = Random.Range(0,2);
+                            Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                            lastRoomInstantiate.Add(rooms[rand]);
+                        }
+                        direction = Random.Range(1, 6);
+                        Debug.Log("Prochaine direction : " + direction);
                     }
                 }
-                Vector2 newPos = new Vector2(transform.position.x, transform.position.y + moveAmount);
-                transform.position = newPos;
-                
-                int rand = Random.Range(2, 4);
-                Instantiate(rooms[rand], transform.position, Quaternion.identity);
-                lastRoomInstantiate.Add(rooms[rand]);
-                
-                direction = Random.Range(1, 6);
+                else
+                {
+                    direction = Random.Range(1, 5);
+                    Debug.Log("Prochaine direction : " + direction);
+                }
             }
-            
-            else {
+            else
+            {
                 // Stop level generation
                 stopGeneration = true;
-            } 
+            }
         }
     }
 }
--- RogueLike Bigouden/Assets/Prefabs/AUC_Prefabs/ProcGen/RoomType.cs
+++ RogueLike Bigouden/Assets/Prefabs/AUC_Prefabs/ProcGen/RoomType.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RoomType : MonoBehaviour
 {
     public int type;
+    public int Size;
     
     
     // Start is called before the first frame update
