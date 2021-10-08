e740f83ac589f4af7457574dab26b5ff5d40130a
BaptisteV <b.vall@rubika-edu.com>
2021-10-08 21:47:34 +0200

    Push
    Push

---
+++ RogueLike Bigouden/Assets/Scripts/BAV_Script/PlayerInputHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerConfiguration playerConfig;
+    private Mover mover;
+
+    [SerializeField]
+    private MeshRenderer playerMesh;
+
+    private BAV_PlayerController controls;
+
+    private void Awake()
+    {
+        mover = GetComponent<Mover>();
+        controls = new BAV_PlayerController();
+    }
+
+    public void InitializePlayer(PlayerConfiguration config)
+    {
+        playerConfig = config;
+        playerMesh.material = config.playerMaterial;
+        config.Input.onActionTriggered += Input_onActionTriggered;
+    }   
+
+    private void Input_onActionTriggered(CallbackContext obj)
+    {
+        
+        if (obj.action.name == controls.Player.Move.name)
+        {
+            OnMove(obj);
+        }
+        
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        if(mover != null)
+            mover.SetInputVector(context.ReadValue<Vector2>());
+    }
+
+}
