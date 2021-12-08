1819fee355341e552c3ef4d003de686f15b931b7
BaptisteV98 <80100088+BaptisteV98@users.noreply.github.com>
2021-12-08 06:01:20 +0100

    Push Animator Pour les piliers

--- RogueLike Bigouden/Assets/Scripts/BAV_Script/Props/AnimatorManager_Props.cs
+++ RogueLike Bigouden/Assets/Scripts/BAV_Script/Props/AnimatorManager_Props.cs
@@ -7,25 +7,70 @@ public class AnimatorManager_Props : MonoBehaviour
 {
     public List<ProjectorPropsProperties> props;
 
+    //private----------
+    //Props Environnement----------
+    [SerializeField] private Props_EnvironnementManager firstPillar;
+    [SerializeField] private Props_EnvironnementManager endPillar;
+
+    [SerializeField] int projectorCount;
+    [SerializeField] int pillarCount;
+    [SerializeField] int rackCount;
+    [SerializeField] int firstPillarF;
+    [SerializeField] int endPillarF;
+
 
     public void Start()
     {
-        
+        foreach (ProjectorPropsProperties t in props)
+        {
+            pillarCount = t.pillarCount;
+            projectorCount = t.projectorCount;
+            rackCount = t.rackCount;
+            endPillarF = t.pillarCount - 1;
+        }
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
         LaunchAnimation();
     }
 
     public void LateUpdate()
     {
-        
     }
 
     public void LaunchAnimation()
     {
-        
+        firstPillar = null;
+        endPillar = null;
+        foreach (ProjectorPropsProperties t in props)
+        {
+            //Get the privateFloat of Pillar;
+
+            //Check the ID of the Pillar to setup the Function
+            firstPillar = t.listOfPillar[firstPillarF];
+            endPillar = t.listOfPillar[endPillarF];
+
+            if (firstPillar.hurt && firstPillar.incrementFloat >= t.lifeLaunchProjectorAnim)
+            {
+                t.animatorProjectorList[firstPillarF].SetTrigger("Fall");
+            }
+
+            if (endPillar.hurt && endPillar.incrementFloat >= t.lifeLaunchProjectorAnim)
+            {
+                t.animatorProjectorList[endPillarF].SetTrigger("Fall");
+            }
+
+            if (firstPillar.hurt && firstPillar.incrementFloat >= t.lifeLaunchRackAnim)
+            {
+                t.animatorRackList[endPillarF].SetTrigger("Fall");
+            }
+            
+            if (endPillar.hurt && endPillar.incrementFloat >= t.lifeLaunchRackAnim)
+            {
+                t.animatorRackList[endPillarF].SetTrigger("Fall");
+            }
+        }
     }
 }
 
@@ -35,8 +80,10 @@ public class ProjectorPropsProperties
 {
     public int pillarCount;
     public List<Props_EnvironnementManager> listOfPillar;
+    public int lifeLaunchProjectorAnim;
     public int projectorCount;
     public List<Animator> animatorProjectorList;
+    public int lifeLaunchRackAnim;
     public int rackCount;
     public List<Animator> animatorRackList;
 }
