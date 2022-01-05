0ea2578a6436db96dcfa12f2c020a21ff92bbc08
BaptisteV98 <80100088+BaptisteV98@users.noreply.github.com>
2022-01-05 02:16:35 +0100

    Push Table Inte

--- RogueLike Bigouden/Assets/Scripts/BAV_Script/Ennemie/Props_EnvironnementManager.cs
+++ RogueLike Bigouden/Assets/Scripts/BAV_Script/Ennemie/Props_EnvironnementManager.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+using DG.Tweening;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Props_EnvironnementManager : MonoBehaviour
@@ -16,93 +17,30 @@ public class Props_EnvironnementManager : MonoBehaviour
         set => propsData.lifePointSO = lifePointSO;
     }
 
-    private int damageSO
-    {
-        get => propsData.damageSO;
-        set => propsData.damageSO = damageSO;
-    }
-
-    private int counterDamageForLaunchAnimSO
-    {
-        get => propsData.counterDamageForLaunchAnimSO;
-        set => propsData.counterDamageForLaunchAnimSO = counterDamageForLaunchAnimSO;
-    }
-
-    private float timeBeforeLaunchAnimationSO
-    {
-        get => propsData.timeBeforeLaunchAnimationSO;
-        set => propsData.timeBeforeLaunchAnimationSO = timeBeforeLaunchAnimationSO;
-    }
-
-    private float animationSpeedSO
-    {
-        get => propsData.animationSpeedSO;
-        set => propsData.animationSpeedSO = animationSpeedSO;
-    }
-
-    private bool isDestructibleSO
-    {
-        get => propsData.isDestructibleSO;
-        set => propsData.isDestructibleSO = isDestructibleSO;
-    }
-
-    private bool isDamageSO
-    {
-        get => propsData.isDamageSO;
-        set => propsData.isDamageSO = isDamageSO;
-    }
-
-    private bool isDestructSO
-    {
-        get => propsData.isDestructSO;
-        set => propsData.isDestructSO = isDestructSO;
-    }
-
-    private bool isTriggerSO
-    {
-        get => propsData.isTriggerSO;
-        set => propsData.isTriggerSO = isTriggerSO;
-    }
-
     private Sprite spritePropsSO
     {
         get => propsData.spritePropsSO;
         set => propsData.spritePropsSO = spritePropsSO;
     }
-    
+
     private Color spriteHitColorSO
     {
         get => propsData.spriteHitColorSO;
         set => propsData.spriteHitColorSO = spriteHitColorSO;
     }
+    
+
+    [Header("Value to communicate to the Rack")]
     //Private Increment Value
     public float incrementFloat;
 
     // Common Int
     public int lifePoint; // Point de vie du props.
-    public int damage; // Damage of the Props.
-    public int counterDamageForLaunchAnim; // Damage of the Props.
-
-    // Common Float
-    public float timeBeforeLaunchAnimation; // Delay avant qu'on lance l'animation.
-    public float animationSpeed; // Vitesse de l'animator.
-
-    // Common Bool
-    public bool isDestructible; // Le props est-il destructible ?
-    public bool isDamage; //Le props est-il endommagé ?
-    public bool isDestruct; //Le props est-il detruit ?
-    public bool isTrigger; //Le Collider est-il trigger ?
 
     //Common Animation.
     public Sprite spriteProps;
-    public List<Collider2D> propsCollider;
     public Color hitcolor;
-    
-    //Common 
-    public bool needLaunchAnim;
-    public float animCounter;
-    public float reachAnimCounter;
-    public List<Animation> animStockage;
+
 
     public bool isProjector;
     //public List<Project> projector;
@@ -113,36 +51,18 @@ public class Props_EnvironnementManager : MonoBehaviour
     [SerializeField] private float counterBeforeReset;
     private SpriteRenderer spriteRenderer;
     public int incrementDamage;
-    
+
     //Common Sprite--
     public Sprite baseSpriteData;
     public List<Sprite> spriteSwap;
     [Range(0, 2)] public int variantNumber;
-
-    //SpriteRenderer
-    [Header("Tcheker")] public bool useBaseSprite;
-    public bool useRandomNumber;
-    public bool updateInRealtime;
-    [Header("Component of Props")] public SpriteRenderer baseSprite;
-    public SpriteRenderer variantSprite;
-    [Header("Color of the props")] public Color propsColor;
-    public Color emissiveColor;
-    [Range(-10, 10)] public float intensity;
+    
 
     #endregion
 
     private void Start()
     {
         lifePoint = lifePointSO;
-        damage = damageSO;
-        counterDamageForLaunchAnim = counterDamageForLaunchAnimSO;
-        counterDamageForLaunchAnim = counterDamageForLaunchAnimSO;
-        timeBeforeLaunchAnimation = timeBeforeLaunchAnimationSO;
-        animationSpeed = animationSpeedSO;
-        isDestructible = isDestructibleSO;
-        isDamage = isDamageSO;
-        isDestruct = isDestructSO;
-        isTrigger = isTriggerSO;
         spriteProps = spritePropsSO;
         hitcolor = spriteHitColorSO;
     }
@@ -159,16 +79,14 @@ public class Props_EnvironnementManager : MonoBehaviour
             //Si le joueur spamm sur le props 
             if (lifePoint <= -3)
             {
-                isDestruct = true;
                 Destroy(gameObject);
             }
             //Si le joueur ne spamm pas 
             else
             {
-                isDestruct = true;
                 //animatorProps.SetBool("Destroy", destroyAnim);
                 Destroy(gameObject, 3f);
-            } 
+            }
         }
     }
 
@@ -183,32 +101,16 @@ public class Props_EnvironnementManager : MonoBehaviour
     {
         if (hurt)
         {
-            SpriteSwap();
-            //Launch jiggle Animation si Props Projecteur.
-            CounterBeforeReset();
+            StartCoroutine(HurtColorTint());
         }
     }
 
-    public void SpriteSwap()
-    {
-        if (hurt)
-        {
-            gameObject.GetComponentInChildren<SpriteRenderer>().color = hitcolor;
-        }
-        else
-        {
-            gameObject.GetComponentInChildren<SpriteRenderer>().color = resetColor;
-        }
-    }
 
-    public void CounterBeforeReset()
+    IEnumerator HurtColorTint()
     {
-        counterBeforeReset += Time.deltaTime;
-        if (counterBeforeReset > 0.2f)
-        {
-            gameObject.GetComponentInChildren<SpriteRenderer>().color = resetColor;
-            counterBeforeReset = 0f;
-            hurt = false;
-        }
+        gameObject.GetComponentInChildren<SpriteRenderer>().DOColor(hitcolor, 0f);
+        yield return new WaitForSeconds(0.2f);
+        gameObject.GetComponentInChildren<SpriteRenderer>().DOColor(resetColor, 0f);
+        hurt = false;
     }
 }
