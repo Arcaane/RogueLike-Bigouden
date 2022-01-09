4ba248c1c0c117338b2fa68617179ecdd7943cb1
Aurélien <71558277+Arcaane@users.noreply.github.com>
2022-01-09 22:44:03 +0100

    Revert "Update EnnemyStatsManager.cs"
    This reverts commit 429e934c8995ebd94f5b95ac5c93787d36544d0f.

--- RogueLike Bigouden/Assets/EnnemyStatsManager.cs
+++ RogueLike Bigouden/Assets/EnnemyStatsManager.cs
@@ -285,5 +285,14 @@ public class EnnemyStatsManager : MonoBehaviour
             StartCoroutine(HurtColorTint());
         }
     }
+
+    IEnumerator HurtColorTint()
+    {
+        gameObject.GetComponentInChildren<SpriteRenderer>().DOColor(hitcolor, 0f);
+        yield return new WaitForSeconds(0.2f);
+        gameObject.GetComponentInChildren<SpriteRenderer>().DOColor(notHurtColor, 0f);
+    }
+    
+    
     #endregion
 }
