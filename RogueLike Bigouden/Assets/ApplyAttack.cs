82ff21f9cf54fa33477cb2ee2bf2a491869ce32a
Djason Nathiez <d.nathiez@rubika-edu.com>
2022-01-18 13:36:58 +0100

    Update ApplyAttack.cs

--- RogueLike Bigouden/Assets/ApplyAttack.cs
+++ RogueLike Bigouden/Assets/ApplyAttack.cs
@@ -134,7 +134,7 @@ public class ApplyAttack : MonoBehaviour
             {
                 int roll = UnityEngine.Random.Range(0, 100);
 
-                if (roll < 30)
+                if (roll < 15)
                 {
                     Debug.Log("Heal by medic with " + roll + " roll");
                     PlayerStatsManager.playerStatsInstance.lifePoint += mediKitHeal;
